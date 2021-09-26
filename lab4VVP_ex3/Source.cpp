92b0138cc57de19966e17800cea3cb0861e9840a
HYPERPC <HYPERPC@DESKTOP-9FMUOVG>
2021-09-26 18:01:07 +0300

    Добавьте файлы проекта.

---
+++ lab4VVP_ex3/Source.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float a, b, SredArifm;
+	a = 5;
+	b = 4;
+	SredArifm = (a + b) / 2;
+	printf("SredArifm = ");
+	printf("%f\n", SredArifm);
+}
